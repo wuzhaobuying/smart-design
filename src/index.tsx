8173fdf2004d0aa7a637c1f338160b16602e5477
smart <yecongmin@bytedance.com>
2020-06-05 10:56:57 +0800

    删除一些冗余

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Button, { ButtonType, ButtonSize } from "./components/Button/button";
-import Menu from "./components/Menu/menu";
-import SubMenu from "./components/Menu/submenu";
-import MenuItem from "./components/Menu/menuitem";
-import Icon from "./components/Icon/icon";
 import "./styles/index.scss";
 
 ReactDOM.render(
