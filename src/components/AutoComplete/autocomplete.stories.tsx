21ecdd82e9da666c6a744ea59cc7b112e3afd609
wuzhaobuying <a2330089@qq.com>
2020-06-23 11:08:53 +0800

    增加eslint、stylelint、prettier环境配置

--- src/components/AutoComplete/autocomplete.stories.tsx
+++ src/components/AutoComplete/autocomplete.stories.tsx
@@ -1,36 +1,36 @@
-import React from "react";
+import * as React from "react";
 import AutoComplete, { DataSourceType } from "./autocomplete";
 
 export default {
-  component: AutoComplete,
-  title: "AutoComplete",
-  decorators: [
-    (storyFn) => (
-      <div style={{ margin: "0 10px", width: "200px" }}>{storyFn()}</div>
-    ),
-  ],
+	component: AutoComplete,
+	title: "AutoComplete",
+	decorators: [
+		(storyFn) => (
+			<div style={{ margin: "0 10px", width: "200px" }}>{storyFn()}</div>
+		),
+	],
 };
 
 const handleFetch = (query: string) => {
-  return fetch(`https://api.github.com/search/users?q=${query}`)
-    .then((res) => res.json())
-    .then(({ items }) => {
-      console.log(items);
-      return items
-        .slice(0, 10)
-        .map((item: any) => ({ value: item.login, ...item }));
-    });
+	return fetch(`https://api.github.com/search/users?q=${query}`)
+		.then((res) => res.json())
+		.then(({ items }) => {
+			console.log(items);
+			return items
+				.slice(0, 10)
+				.map((item: any) => ({ value: item.login, ...item }));
+		});
 };
 
 const onSelect = (item: DataSourceType) => {
-  console.log(item.value);
+	console.log(item.value);
 };
 
 export const autoComplete = () => (
-  <>
-    <AutoComplete
-      fetchSuggestions={handleFetch}
-      onSelect={onSelect}
-    ></AutoComplete>
-  </>
+	<>
+		<AutoComplete
+			fetchSuggestions={handleFetch}
+			onSelect={onSelect}
+		></AutoComplete>
+	</>
 );
--- src/components/AutoComplete/index.tsx
+++ src/components/AutoComplete/index.tsx
@@ -1,3 +1,2 @@
 import AutoCompelete from "./autocomplete";
-
 export default AutoCompelete;
